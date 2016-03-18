6499117074344fa02771d0d2ca1828b5c15534cd
Victoria <tapia.victoria5070@gmail.com>
2016-03-17 18:41:07 -0700

    created setup for react app

---
+++ main.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(<app />, document.getElementById('app'))
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(<App />, document.getElementById('app'))
