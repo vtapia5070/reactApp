c15760fd975574c23f12c69df9819045c7ca24fc
Victoria <tapia.victoria5070@gmail.com>
2016-03-17 21:16:07 -0700

    demo: component lifecycles (mount, willMount, unmount)

--- App.js
+++ App.js
@@ -2,23 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {val: 0};
+    this.update = this.update.bind(this);
+  }
+  update() {
+    this.setState({val: this.state.val +1 })
+  }
+  componentWillMount() {
+    console.log("mounting");
+  }
+
   render(){
+    console.log("rendering!");
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+  componentDidMount() {
+    console.log("component Did mount");
+  }
+  componentWillUnmount() {
+    console.log("bye");
+  }
+}
+
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+  }
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render() {
     return (
       <div>
-        <Button>I <Heart /> React</Button>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
       </div>
     )
   }
 }
 
-class Button extends React.Component {
-  render() {
-    return (
-      <button>{this.props.children}</button>
-    )
-  }
-}
-
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
-
-export default App
+export default Wrapper
