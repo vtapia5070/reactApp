191ffae1de0102119f529a548059faeb2fa47441
Victoria <tapia.victoria5070@gmail.com>
2016-03-17 20:23:34 -0700

    create parent, or composite component with child component

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ class App extends React.Component {
   constructor () {
     super();
     this.state = {
-      txt: 'this is the state txt',
-      cat: 0
+      txt: ''
     }
+    this.update = this.update.bind(this);
   }
   update (e) {
     this.setState({txt: e.target.value});
@@ -15,11 +15,20 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
     )
   }
+}
+
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  )
 }
 
 export default App
