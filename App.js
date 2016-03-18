10e663b6c3be289c4b59e38d4f52766ad3474cd6
Victoria <tapia.victoria5070@gmail.com>
2016-03-17 20:04:00 -0700

    implemented component that updates state with input txt

--- App.js
+++ App.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
-/* stateful component */
 class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+  update (e) {
+    this.setState({txt: e.target.value});
+  }
   render(){
-    let txt = this.props.txt
-    return <h1>{txt}</h1>
-    // return <h1>{this.props.txt}</h1>
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "this is the default txt"
-}
-/* Uncomment below for stateless component */
-// const App = () => <h1>I'm a stateless component!</h1>
-
-ReactDOM.render(
-  <App cat={5} />,
-  document.getElementById('app')
-);
-
-// export default App
+export default App
